c1bb4ef797d308f056115b0366280ac4b279fbcf
Dino Dunvjak <dinoduvnjak@gmail.com>
2018-06-08 08:19:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,3 +35,15 @@ function removeElementFromBeginningOfArray (array) {
   array.slice(1);
   return array;
 }
+
+function destructivelyRemoveElementFromEndOfArray (array) {
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray (array) {
+  array.slice(0, array.length - 1);
+  return array;
+}
+
+
